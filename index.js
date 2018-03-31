4732a56a0c2715c7786729b2390721be8b33681d
Frank Rose <frankrosework@gmail.com>
2018-03-31 01:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-var breakOut = function(array, changeValue, stopValue) {};
+var breakOut = function(array, changeValue, stopValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === stopValue) {
+      break;
+    }
+
+    array[i] = changeValue;
+  }
+};
 
 var keepGoing = function(array, changeValue, skipValue) {};
 
