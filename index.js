bc31642d817af3fa8fcb4dabe36f8c7271a38c8f
Frank Rose <frankrosework@gmail.com>
2018-03-31 01:03:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,6 +8,14 @@ var breakOut = function(array, changeValue, stopValue) {
   }
 };
 
-var keepGoing = function(array, changeValue, skipValue) {};
+var keepGoing = function(array, changeValue, skipValue) {{
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue;
+    }
+
+    array[i] = changeValue;
+  }
+};
 
 var findBy = function(array, findFn) {};
